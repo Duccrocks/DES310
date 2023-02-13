2aa7acee251461f502e02330f54d3d974b652cd1
Callum I <2101493@uad.ac.uk>
2023-02-13 17:43:56 +0000

    Squashed commit of the following:
    commit 735822f942dacc2598d708b97370fe5ed8a31c61
    Merge: dba747b 04811bc
    Author: CallumInnes-Abertay <99197999+CallumInnes-Abertay@users.noreply.github.com>
    Date:   Mon Feb 13 17:39:09 2023 +0000
    
        Merge pull request #5 from Duccrocks/new-input
    
        New unity input system with first person movement
    
    commit 04811bc9564d15f6c2c0ee583f1b144c7e62e001
    Author: Callum I <2101493@uad.ac.uk>
    Date:   Mon Feb 13 17:37:39 2023 +0000
    
        Reformat
    
    commit 20ced94cce2837d7082df124438c3eea4660001d
    Author: Callum I <2101493@uad.ac.uk>
    Date:   Mon Feb 13 17:30:04 2023 +0000
    
        Better event cleanup and further gizmos
    
    commit 6e3bc7561f48ff168f0ffc88e525c675aad561f5
    Author: Callum I <2101493@uad.ac.uk>
    Date:   Sat Feb 11 14:37:02 2023 +0000
    
        New Input system
    
        Fixed movement, Gravity, Jump
        Range setting for serialized fields.
        New Input system supporting different devices with input manager class handling it.
    
    commit dba747b1a46df675fecbcedfff70e02292610cb3
    Merge: a39020d 15595a4
    Author: CiaranMcl <71699676+CiaranMcl@users.noreply.github.com>
    Date:   Mon Feb 6 19:48:54 2023 +0000
    
        Merge pull request #4 from Duccrocks/RadarMaze
    
        Radar Maze proto
    
    commit a39020d58204d0a7278e12f5608cb05a8e3565b8
    Merge: 1dd91b5 c5b3956
    Author: CallumInnes-Abertay <99197999+CallumInnes-Abertay@users.noreply.github.com>
    Date:   Fri Feb 3 20:32:33 2023 +0000
    
        Merge pull request #3 from Duccrocks/git-lfs-test
    
        .gitignore is now working
    
    commit c5b3956eff1fb5e9c952505c22ad1da07ac12355
    Author: Callum I <2101493@uad.ac.uk>
    Date:   Fri Feb 3 19:55:59 2023 +0000
    
        .gitignore is now working
    
    commit 1dd91b53cc77a5464560e26bbd0dc5f7a9c7bd61
    Author: Callum I <2101493@uad.ac.uk>
    Date:   Fri Feb 3 18:19:34 2023 +0000
    
        Update .gitignore and set up LFS

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private CameraController cameraController;
+
+    //Reference to all player controls.
+    private PlayerControls playerControls;
+    private PlayerMovement playerMovement;
+
+    private void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        cameraController = GetComponentInChildren<CameraController>();
+
+        playerControls = new PlayerControls();
+    }
+
+    private void Start()
+    {
+   
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+        playerControls.Player.Move.performed += GetPlayerMovement;
+        playerControls.Player.Look.performed += GetAxis;
+        playerControls.Player.Jump.performed += Jump;
+    }
+
+    private void OnDisable()
+    {
+        //Personally I don't like memory leaks.
+        playerControls.Disable();
+        playerControls.Player.Move.performed -= GetPlayerMovement;
+        playerControls.Player.Look.performed -= GetAxis;
+        playerControls.Player.Jump.performed -= Jump;
+    }
+
+
+    #region Player Controls
+
+    public void GetPlayerMovement(InputAction.CallbackContext ctx)
+    {
+        playerMovement.InputMovement = ctx.ReadValue<Vector2>();
+    }
+
+    public void GetAxis(InputAction.CallbackContext ctx)
+    {
+        cameraController.InputRotation = ctx.ReadValue<Vector2>();
+    }
+
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started) playerMovement.Jump();
+    }
+
+    #endregion
+}
