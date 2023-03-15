d0d642d8cb62a3ce34d401332b50e304e86cb304
LTreharne <78056359+LTreharne@users.noreply.github.com>
2023-03-15 16:23:58 +0000

    punch mechanic

---
+++ Assets/Scripts/QOS/Punch.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punch : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMaskInteract;
+    [SerializeField] private string excludedLayer;
+    [SerializeField] float attackRange=5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+       
+            RaycastHit hit;
+            var forward = transform.TransformDirection(Vector3.forward);
+            var mask = (1 << LayerMask.NameToLayer(excludedLayer)) | layerMaskInteract.value;
+            
+            if (Physics.Raycast(transform.position, forward, out hit, attackRange, mask))
+            {
+               
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    hit.transform.gameObject.GetComponent<PunchResponse>().Punched(forward);
+                }
+            }
+        }
+    }
+}
