eee6f77100327291fdc21cbdffd770d8ce7af2e9
Callum I <2101493@uad.ac.uk>
2023-03-19 22:55:31 +0000

    Stamina added to sprinting
    Sprinting now uses stamina working off couritines to slowly go down while sprinting and quickly recover when no longer.
    Sprinting is now on down instead of on start for input.
    Made HUD a prefab instead of just crosshair
    Added RequireComponent to some classes to reduce chances of null references.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,28 +4,39 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")] 
-    
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] [Range(1, 5)] private float sprintMultiplier = 2f;
     [SerializeField] private float jumpHeight = 5.0f;
     [SerializeField] private float gravity = -9.81f;
-    [SerializeField] [Range(1, 10)] private float sprintMultiplier = 2f;
 
     [Header("Ground Check")]
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private Transform groundCheck;
     [SerializeField] [Range(0, 1)] private float groundDistance;
-    
+
     private CharacterController controller;
+    private SprintController sprintController;
+    
+    private bool isSprinting;
+    private float sprintSpeed;
+    private float walkSpeed;
     private float yVelocity;
+
 
     private void Start()
     {
+        //Set differing speed values.
+        walkSpeed = speed;
+        sprintSpeed = speed * sprintMultiplier;
+
         controller = GetComponent<CharacterController>();
+        sprintController = GetComponent<SprintController>();
     }
 
     private void Update()
     {
         Gravity();
+        if (isSprinting) StaminaCheck();
     }
 
     private void OnDrawGizmos()
@@ -38,7 +49,8 @@ public class PlayerMovement : MonoBehaviour
 
     public void Movement(Vector2 inputMovement)
     {
-        var move = transform.right * inputMovement.x + transform.forward * inputMovement.y;
+        var playerTransform = transform;
+        var move = playerTransform.right * inputMovement.x + playerTransform.forward * inputMovement.y;
         controller.Move(move * (speed * Time.deltaTime));
     }
 
@@ -63,14 +75,27 @@ public class PlayerMovement : MonoBehaviour
         if (IsGrounded()) yVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
     }
 
+    private void StaminaCheck()
+    {
+        if (sprintController.CurrentStamina <= 0)
+        {
+            StopSprinting();
+            return;
+        }
+
+        if (isSprinting) sprintController.UseStamina();
+    }
+
     public void StartSprinting()
     {
-        speed *= sprintMultiplier;
+        speed = sprintSpeed;
+        isSprinting = true;
     }
 
     public void StopSprinting()
     {
-        speed /= sprintMultiplier;
+        speed = walkSpeed;
+        isSprinting = false;
     }
 
     private bool IsGrounded()
