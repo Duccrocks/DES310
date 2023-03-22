1801ee14756c13d9235641a1ab5ea954dbb44b40
LTreharne <78056359+LTreharne@users.noreply.github.com>
2023-03-22 15:54:53 +0000

    :horse:

--- Assets/Scripts/Radar Maze/Wisps/WispDespawn.cs
+++ Assets/Scripts/Radar Maze/Wisps/WispDespawn.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class WispDespawn : MonoBehaviour
 {
+    
+    private Transform kelpie;
+    private void Start()
+    {
+        kelpie = GameObject.FindGameObjectWithTag("Enemy").transform;
+    }
+    private void Update()
+    {
+        if (kelpie != null)
+        {
+            if (Vector3.Distance(kelpie.position, transform.position) < 8)
+                Destroy(this.gameObject);
+        }
+    }
     // Destroys the wisp when player gets within trigger collider
     void OnTriggerEnter(Collider other)
     {
         bool isActor = other.gameObject.CompareTag("Player");
         if (isActor) Destroy(this.gameObject);
     }
+
 }
