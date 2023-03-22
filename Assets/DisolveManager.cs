9cde7391cd8dd154c475cfc4d38cdad9cbeffe82
LTreharne <78056359+LTreharne@users.noreply.github.com>
2023-03-22 13:29:06 +0000

    directional fade

--- Assets/DisolveManager.cs
+++ Assets/DisolveManager.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class DisolveManager : MonoBehaviour
 {
     Renderer renderer;
+    MeshFilter meshFilter;
     float clip;
     // Start is called before the first frame update
     void Start()
     {
         renderer = GetComponent<Renderer>();
+        meshFilter = GetComponent<MeshFilter>();
         clip = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float height = meshFilter.mesh.bounds.extents.y;
         clip -= Time.deltaTime/5.0f;
         clip = Mathf.Max(clip, 0);
         renderer.material.SetFloat("_alphaClip", clip);
+        renderer.material.SetFloat("_height", height);
     }
 
     private void OnApplicationQuit()
