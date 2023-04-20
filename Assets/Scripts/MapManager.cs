52f9ef15e271624c2db91d2a1c2a063582f38346
Callum I <2101493@uad.ac.uk>
2023-04-20 15:36:37 +0100

    Refactoring

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -3,65 +3,52 @@ using UnityEngine.UI;
 
 public class MapManager : MonoBehaviour
 {
-    [SerializeField] private Image BottomLeftImage;
-    [SerializeField] private Image BottomRightImage;
-    [SerializeField] private Image TopLeftImage;
-    [SerializeField] private Image TopRightImage;
-    [SerializeField] private Image panel;
-    [SerializeField] private GameObject hud;
-    private int artifactNum;
+    [Header("Map Images")] 
+    [SerializeField] private Image bottomLeftImage;
+    [SerializeField] private Image bottomRightImage;
+    [SerializeField] private Image topLeftImage;
+    [SerializeField] private Image topRightImage;
+    
+    [Header("HUD")]
+    [SerializeField] private Image panel; 
+    [SerializeField] private GameObject hudPanel;
+
+    public bool bottomLeftCollected;
+    private bool bottomRightCollected;
+    private bool topLeftCollected;
+    private bool topRightCollected;
 
     private bool mapOpened;
 
-    public bool bottomRightCollected;
-    public bool bottomLeftCollected;
-    public bool TopRightCollected;
-    public bool TopLeftCollected;
-
+    private int artifactNum;
     private void Start()
     {
-        bottomLeftCollected= false;
-        bottomRightCollected= false;
-        TopLeftCollected= false;
-        TopRightCollected= false;
+        bottomLeftCollected = false;
+        bottomRightCollected = false;
+        topLeftCollected = false;
+        topRightCollected = false;
         CloseMap();
-        
     }
-
-    // private void Update()
-    // {
-    //     if (Input.GetKeyDown(KeyCode.M))
-    //     {
-    //         if (mapOpened)
-    //         {
-    //             CloseMap();
-    //             mapOpened = false;
-    //         }
-    //         else
-    //         {
-    //             OpenMap();
-    //             mapOpened = true;
-    //         }
-    //     }
-    // }
 
     public void PieceCollected(int id)
     {
         switch (id)
         {
-            case 0: TopLeftCollected = true;
+            case 0:
+                topLeftCollected = true;
                 break;
-            case 1: TopRightCollected= true;
+            case 1:
+                topRightCollected = true;
                 break;
-            case 2: bottomRightCollected= true;
+            case 2:
+                bottomRightCollected = true;
                 break;
-            default: break;
         }
     }
 
     public void ToggleMap()
     {
-        
+        if (!(bottomLeftCollected || bottomRightCollected || topLeftCollected || topRightCollected)) return;
         if (mapOpened)
         {
             CloseMap();
@@ -73,26 +60,27 @@ public class MapManager : MonoBehaviour
             mapOpened = true;
         }
     }
+    
 
     private void OpenMap()
     {
-        hud.SetActive(false);
+        hudPanel.SetActive(false);
         panel.enabled = true;
 
-        TopLeftImage.enabled = TopLeftCollected;
-        TopRightImage.enabled = TopRightCollected;
-        BottomLeftImage.enabled = bottomLeftCollected;
-        BottomRightImage.enabled = bottomRightCollected;
+        topLeftImage.enabled = topLeftCollected;
+        topRightImage.enabled = topRightCollected;
+        bottomLeftImage.enabled = bottomLeftCollected;
+        bottomRightImage.enabled = bottomRightCollected;
     }
 
     private void CloseMap()
     {
-        hud.SetActive(true);
+        hudPanel.SetActive(true);
 
         panel.enabled = false;
-        TopLeftImage.enabled = false;
-        TopRightImage.enabled = false;
-        BottomLeftImage.enabled = false;
-        BottomRightImage.enabled = false;
+        topLeftImage.enabled = false;
+        topRightImage.enabled = false;
+        bottomLeftImage.enabled = false;
+        bottomRightImage.enabled = false;
     }
 }
