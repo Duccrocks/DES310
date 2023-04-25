f8042c38fd12ab196ea2a8171bdb5ce8eba999d9
LTreharne <78056359+LTreharne@users.noreply.github.com>
2023-04-25 14:30:42 +0100

    changing material as opposed to texture

--- Assets/BookColour.cs
+++ Assets/BookColour.cs
@@ -6,16 +6,4 @@ using UnityEngine.Rendering;
 
 public class BookColour : MonoBehaviour
 {
-
-
-     [SerializeField] public Texture UsingTexture;
- 
-    // Start is called before the first frame update
-    
-
-    // Update is called once per frame
-    void Update()
-    {
-        GetComponent<Renderer>().material.SetTexture("_BaseMap", UsingTexture);
-    }
 }
--- Assets/BookManager.cs
+++ Assets/BookManager.cs
@@ -1,38 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class BookManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private Texture red;
-    [SerializeField] private Texture blue;
-    [SerializeField] private Texture green;
 
-    [SerializeField] private Material redMat;
-    [SerializeField] private Material blueMaterial;
-    [SerializeField] private Material GreenMat;
-
+    [SerializeField] Material[] materials;
     public void Colour()
     {
         BookColour[] booksArray = FindObjectsOfType<BookColour>();
 
         foreach (BookColour book in booksArray)
         {
-            int rand = Random.Range(0, 3);
-            switch (rand) {
-                case 0:
-                    book.gameObject.GetComponent<Renderer>().materials[0] = GreenMat;
-                    break;
-                case 1:
-                    book.gameObject.GetComponent<Renderer>().materials[0] = blueMaterial;
-                    break;
-                case 2:
-                    book.gameObject.GetComponent<Renderer>().materials[0] = redMat;
-                    break;
-                default:
-                    break;
-            }
+            int rand = Random.Range(0, materials.Length);
+            book.gameObject.GetComponent<Renderer>().material = materials[rand];
         }
     }
 }
