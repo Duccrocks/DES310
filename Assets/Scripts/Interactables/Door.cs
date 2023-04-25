a818861e05bd4badae7c75a974fc9c0966f60faa
CiaranMcl <71699676+CiaranMcl@users.noreply.github.com>
2023-04-25 16:53:36 +0100

    Added door prefab with functionality

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -6,13 +6,19 @@ using UnityEngine.Events;
 public class Door : MonoBehaviour, IInteractable
 {
     public UnityEvent doorOpened;
-    private bool doOnce = false;
+    public UnityEvent doorClosed;
+    private bool doorOpen = false;
     public void Interact()
     {
-        if(!doOnce)
+        if (!doorOpen)
         {
             doorOpened?.Invoke();
-            doOnce = true;
+            doorOpen = true;
+        }
+        else 
+        {
+            doorClosed?.Invoke();
+            doorOpen = false;
         }
     }
 }
