6202d6b6611e137aff119a042f35ce3d00c63db8
LTreharne <78056359+LTreharne@users.noreply.github.com>
2023-04-26 13:12:32 +0100

    Fade in inplimented and added to all scenes

--- Assets/Scripts/Library/BookManager.cs
+++ Assets/Scripts/Library/BookManager.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public class BookManager : MonoBehaviour
 {
+
+    private void Start()
+    {
+        LevelManager.instance.SceneLoaded();
+    }
     // Start is called before the first frame update
 
     [SerializeField] Material[] materials;
