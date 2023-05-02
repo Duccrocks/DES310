cc99d72492ce42537c6dbcab75977a72fc601743
LTreharne <78056359+LTreharne@users.noreply.github.com>
2023-05-02 15:53:22 +0100

    Stamia/health bar revamp ... again :gladge:

--- Assets/Scripts/HealthBarManager.cs
+++ Assets/Scripts/HealthBarManager.cs
@@ -5,30 +5,13 @@ using UnityEngine.UI;
 
 public class HealthBarManager : MonoBehaviour
 {
-    [SerializeField] Image[] hearts;
+    [SerializeField] Image bar;
     [SerializeField] PlayerHealth health;
-    public int segs;
+  
     // Start is called before the first frame update
     // Update is called once per frame
     void Update()
     {
-        int current = (int)health.getHealth() / 100;
-        for (int i=0;i<hearts.Length;++i)
-        {
-            
-            if (current>i)
-            {
-                hearts[i].fillAmount = 1;
-            }
-            else if(current<i)
-            {
-                hearts[i].fillAmount = 0;
-            }
-            else if (i==current)
-            {
-                int quad = (int)(health.getHealth()/(100/segs)) % segs;
-                hearts[i].fillAmount = quad * (1.0f/segs);
-            }
-        }
+        bar.fillAmount = health.getHealth() / health.getMaxHealth();
     }
 }
