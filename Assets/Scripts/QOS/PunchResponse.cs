bfc4f858521efd6d44a79ae0df33f5aeeedb8e50
LTreharne <78056359+LTreharne@users.noreply.github.com>
2023-05-03 13:16:44 +0100

    Different knight Behaviors

--- Assets/Scripts/QOS/PunchResponse.cs
+++ Assets/Scripts/QOS/PunchResponse.cs
@@ -1,13 +1,21 @@
-using UnityEngine;
+ using UnityEngine;
 using UnityEngine.AI;
-
+using System.Collections;
 public class PunchResponse : MonoBehaviour
 {
+    public enum KnightType {smol, red, blue, green };
+
     [SerializeField] private float speed;
     [SerializeField] private int health = 3;
     [SerializeField] private float IFrameTime = 2;
-
+    [SerializeField] KnightType type;
     private Vector3 direction;
+
+    private float deathLeway;
+    private float deathlewaytimer;
+
+    [SerializeField] private DisolveManager[] disolveManagers;
+    private bool Punchahble;
 
     private float healthTimer;
     private NavMeshAgent navMeshAgent;
@@ -16,93 +24,135 @@ public class PunchResponse : MonoBehaviour
 
     private void Awake()
     {
+        disolveManagers = GetComponentsInChildren<DisolveManager>();
         RaddollManager = GetComponent<RaddollManager>();
         navMeshAgent = GetComponent<NavMeshAgent>();
+        healthTimer = 2;
+        Punchahble = true;
+        deathLeway = 3f;
+        deathlewaytimer = 0;
     }
 
     // Start is called before the first frame update
     private void Start()
     {
-        healthTimer = 2;
+ 
     }
 
     // Update is called once per frame
     private void Update()
     {
         healthTimer += Time.deltaTime;
-        if (health <= 0)
-            gameObject.SetActive(false);
-        else
-            gameObject.SetActive(true);
+        if (RaddollManager.ragDollEnabled&&RaddollManager.gameObject.GetComponent<Rigidbody>().velocity.sqrMagnitude<0.02)
+        {
+            deathlewaytimer += Time.deltaTime;
+            if (deathlewaytimer>deathLeway)
+            {
+                Punchahble = false;
+                foreach (DisolveManager dis in disolveManagers)
+                {
+                    dis.disolveMult = -1;
+                }
+                Destroy(gameObject, disolveManagers[0].disolveDuration);
+            }
+            
+        }
+
+
+     
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (!collision.transform.CompareTag("Ground"))
-        {
-            //Debug.Log("hit simmin");
-        }
-
-        if (collision.transform.CompareTag("Enemy") || collision.transform.CompareTag("Physics Object"))
-        {
-            if (collision.transform.CompareTag("Physics Object"))
+        if (Punchahble) { 
+            if (collision.transform.CompareTag("Enemy") || collision.transform.CompareTag("Physics Object"))
             {
-                if (collision.gameObject.GetComponent<Rigidbody>().velocity.magnitude < 0.25)
+                if (collision.transform.CompareTag("Physics Object"))
                 {
-                    return;
+                    if (collision.gameObject.GetComponent<Rigidbody>().velocity.magnitude < 0.25)
+                    {
+                        return;
+                    }
+                }
+                Debug.Log("hit an enemy gamer");
+
+                if (healthTimer > IFrameTime)
+                {
+                    if (collision.transform.CompareTag("Enemy")&&type==KnightType.green)
+                    {
+                        return;
+                    }
+                    health--;
+                    healthTimer = 0;
+                    if (health==0)
+                    {
+                        var dir = Vector3.Normalize(transform.position - collision.transform.position) * speed * 0.2f;
+
+                        var collisionPunchResponse = GetComponentInParent<PunchResponse>();
+                        collisionPunchResponse.DestroyAI();
+
+                        var collisionRagdoll = collisionPunchResponse.gameObject.GetComponent<RaddollManager>();
+                        flying(dir, collisionRagdoll);
+                    }
+                    
                 }
             }
-            Debug.Log("hit an enemy gamer");
-
-            if (healthTimer > IFrameTime)
-            {
-                health--;
-                healthTimer = 0;
-                var dir = Vector3.Normalize(transform.position - collision.transform.position) * speed * 0.2f;
-
-                var collisionPunchResponse = GetComponentInParent<PunchResponse>();
-                collisionPunchResponse.DestroyAI();
-
-                var collisionRagdoll = collisionPunchResponse.gameObject.GetComponent<RaddollManager>();
-                collisionRagdoll.ragDollEnabled = true;
-                collisionRagdoll.EnableRagdoll();
-
-                foreach (var body in collisionRagdoll.Rigidbodies)
-                {
-                    body.AddForceAtPosition(dir, new Vector3(0, 0, 0));
-                }
-            }
-        }
-
-        if (collision.transform.CompareTag("Wisp"))
-        {
-            var xScale = collision.transform.localScale.x;
-
-            var dir = gameObject.GetComponent<Rigidbody>().velocity;
-
-            dir.Normalize();
-            dir *= speed;
-            gameObject.GetComponent<Rigidbody>().AddForceAtPosition(-dir, new Vector3(0, 0, 0));
         }
     }
 
     public void Punched(Vector3 axis)
     {
-        DestroyAI();
-        direction = axis * speed;
-        //GetComponent<Rigidbody>().AddForceAtPosition(direction, new Vector3(0, 0, 0));
-        RaddollManager.ragDollEnabled = true;
-        RaddollManager.EnableRagdoll();
+        if (type ==KnightType.green)
+        {
+            return;
+        }
+        if (Punchahble)
+        {
+            health--;
+            if (health == 0)
+            {
+                deathlewaytimer = 0;
+                DestroyAI();
+                direction = axis * speed;     
+                flying(direction, RaddollManager);
+            }
+            else
+            {
+                direction = axis * speed;
+                StartCoroutine("pushBack", direction);
+            }
+        }
 
-        foreach (var body in RaddollManager.Rigidbodies)
+    }
+
+    private void flying(Vector3 dir, RaddollManager raddollManager)
+    {
+        raddollManager.ragDollEnabled = true;
+        raddollManager.EnableRagdoll();
+
+        foreach (var body in raddollManager.Rigidbodies)
         {
             body.AddForceAtPosition(direction, new Vector3(0, 0, 0));
         }
-    }
+        GetComponent<Rigidbody>().isKinematic = true;
 
+    }
+    IEnumerator pushBack(Vector3 dir)
+    {
+        navMeshAgent.enabled = false;
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.AddForceAtPosition(direction * 0.5f, new Vector3(0, 0, 0));
+        body.constraints = RigidbodyConstraints.FreezePositionY;
+        body.freezeRotation = true;
+        yield return new WaitForSeconds(1);
+        navMeshAgent.enabled = true;
+        body.freezeRotation = false;
+        body.constraints = RigidbodyConstraints.None;
+    }
     public void DestroyAI()
     {
         Destroy(navMeshAgent);
         Destroy(GetComponent<MQSAI>());
     }
+
 }
--- Assets/Scripts/Shaders/DisolveManager.cs
+++ Assets/Scripts/Shaders/DisolveManager.cs
@@ -7,19 +7,22 @@ public class DisolveManager : MonoBehaviour
     Renderer renderer;
     MeshFilter meshFilter;
     public float clip;
+    public float disolveDuration;
+    public int disolveMult;
     // Start is called before the first frame update
     void Start()
     {
         renderer = GetComponent<Renderer>();
         meshFilter = GetComponent<MeshFilter>();
         clip = 1;
+        disolveMult = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
        // float height = meshFilter.mesh.bounds.extents.y;
-        clip -= Time.deltaTime*1/2.0f;
+        clip -= (Time.deltaTime/disolveDuration)*disolveMult;
         clip = Mathf.Max(clip, 0);
         renderer.material.SetFloat("_alphaClip", clip);
         //renderer.material.SetFloat("_height", height);
