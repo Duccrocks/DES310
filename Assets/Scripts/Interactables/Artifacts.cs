1d25d92a5c61ef2683dd7d5281c6826e224afc91
CiaranMcl <71699676+CiaranMcl@users.noreply.github.com>
2023-05-16 13:28:04 +0100

    Refactored kelpie difficulty code

--- Assets/Scripts/Interactables/Artifacts.cs
+++ Assets/Scripts/Interactables/Artifacts.cs
@@ -18,11 +18,12 @@ public class Artifacts : MonoBehaviour, IInteractable
 
     public void Interact()
     {
+        if (kelpie) kelpie.GetComponent<KelpieAI>().IncreaseDiff();
+
         try
         {
             RadarGameManager.Instance.ArtifactObtained(artifactID);
             AudioManager.Instance.PlaySoundOnce(audioClip);
-            if (kelpie) kelpie.GetComponent<KelpieAI>().IncreaseDiff();
         }
         catch (NullReferenceException e)
         {
